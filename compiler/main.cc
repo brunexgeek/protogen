df87e146bc7e6e856608c36b0868beca3581c6a0
Bruno Ribeiro <brunexgeek@users.noreply.github.com>
2018-12-17 02:13:31 -0200

    Create functions for name generation

--- compiler/main.cc
+++ compiler/main.cc
@@ -27,6 +27,16 @@ static std::string toUpper( const std::string &value )
 }
 
 
+static std::string fieldStorage( const Field &field )
+{
+    return toLower(field.name) + "_S";
+}
+
+static std::string fieldFlags( const Field &field )
+{
+    return toLower(field.name) + "_F";
+}
+
 static std::string fieldNativeType( const Field &field )
 {
     switch (field.type)
@@ -65,7 +75,7 @@ static std::string fieldNativeType( const Field &field )
             return "std::string&";
 
         case protogen::TYPE_MESSAGE:
-            return field.typeName + "__type<0>";
+            return field.typeName;
 
         default:
             throw protogen::exception("Invalid field type");
@@ -114,21 +124,31 @@ static void generateFunctions( std::ostream &out, const Field &field )
 {
     std::string type = fieldNativeType(field);
     std::string name = toLower(field.name);
+    std::string storage = fieldStorage(field);
 
     bool ref = true;
 
     // constant getter
     out << "\tconst " << type << ((ref) ? "& " : " ") << name << "() const { return ";
     if (field.type == protogen::TYPE_MESSAGE) out << '*';
-    out << name << "__; }\n";
-    // mutable getter
-    out << "\t" << type << ((ref) ? "& " : " ") << name << "() { return ";
-    if (field.type == protogen::TYPE_MESSAGE) out << '*';
-    out << name << "__; }\n";
+    out << storage << "; }\n";
+
     // setter
     out << "\tvoid " << name << "(const " << type << " &value) {";
     if (field.type == protogen::TYPE_MESSAGE) out << '*';
-    out << name << "__ = value; }";
+    out << storage << " = value; }\n";
+
+    // additional string setter
+    if (field.type == protogen::TYPE_STRING)
+    {
+        out << "\tvoid " << name << "(const char *value) { " << storage << " = value; }\n";
+    }
+
+    // returns the field index
+    out << "\tint index_" << name << "() const { return " << field.index << "; }\n";
+
+    // returns the field name
+    out << "\tstd::string name_" << name << "() const { return \"" << field.name << "\"; }\n";
 
     out << '\n';
 }
@@ -137,7 +157,7 @@ static void generateFunctions( std::ostream &out, const Field &field )
 static void generateDeclarations( std::ostream &out, const Field &field )
 {
     std::string type = fieldNativeType(field);
-    std::string name = toLower(field.name);
+    std::string name = fieldStorage(field);
     // constant getter
     out << "\tconst " << type << ((field.type == protogen::TYPE_STRING) ? "& " : " ") << name << "() const;\n";
     // mutable getter
@@ -152,24 +172,23 @@ static void generateVariable( std::ostream &out, const Field &field )
     // storage variable
     out << '\t' << fieldNativeType(field);
     if (field.type == protogen::TYPE_MESSAGE) out << '*';
-    out << ' ' << field.name << "__;\n";
+    out << ' ' << fieldStorage(field) << ";\n";
     // flags variable
-    out << "\tuint32_t " << field.name << "__flags;\n";
+    out << "\tuint32_t " << fieldFlags(field) << ";\n";
 }
 
 
 static void generateCtor( std::ostream &out, const Message &message )
 {
-    std::string className = message.name + "__type";
-    out << '\t' << className << "() {\n";
+    out << '\t' << message.name << "() {\n";
 
     for (auto it = message.fields.begin(); it != message.fields.end(); ++it)
     {
         if (it->type == protogen::TYPE_STRING) continue;
-        out << "\t\t" << it->name << "__ = ";
+        out << "\t\t" << fieldStorage(*it) << " = ";
 
         if (it->type == protogen::TYPE_MESSAGE)
-            out << "new " << it->typeName << "__type<0>();\n";
+            out << "new " << it->typeName << "();\n";
         else
             out << defaultValue(*it) << ";\n";
     }
@@ -180,24 +199,38 @@ static void generateCtor( std::ostream &out, const Message &message )
 
 static void generateDtor( std::ostream &out, const Message &message )
 {
-    std::string className = message.name + "__type";
-    out << "\tvirtual ~" << className << "() {\n";
+    out << "\tvirtual ~" << message.name << "() {\n";
 
     for (auto it = message.fields.begin(); it != message.fields.end(); ++it)
     {
         if (it->type != protogen::TYPE_MESSAGE) continue;
-        out << "\t\tdelete " << it->name << "__;\n";
+        out << "\t\tdelete " << fieldStorage(*it) << ";\n";
     }
 
     out << "\t}\n";
 }
 
 
+static void generateHasField( std::ostream &out, const Field &field )
+{
+    out << "\tbool has_" << field.name << "() const { return " << fieldFlags(field) << " & 0x01; }\n";
+}
+
+static void generateClearField( std::ostream &out, const Field &field )
+{
+    out << "\tvoid clear_" << field.name << "() { " << fieldStorage(field) << " = ";
+
+    if (field.type == protogen::TYPE_MESSAGE)
+        out << "NULL; ";
+    else
+        out << defaultValue(field) << "; ";
+    out << fieldFlags(field) << " &= ~0x01; }\n";
+}
+
+
 static void generateMessage( std::ostream &out, const Message &message )
 {
-    std::string className = message.name + "__type";
-
-    out << "\ntemplate<int> class " << className << " {\nprivate:\n";
+    out << "\nclass " << message.name << " {\nprivate:\n";
 
     for (auto it = message.fields.begin(); it != message.fields.end(); ++it)
     {
@@ -209,17 +242,17 @@ static void generateMessage( std::ostream &out, const Message &message )
     // default constructor
     generateCtor(out, message);
     // copy constructor
-    out << '\t' << className << "(const " << className << " &obj) {}\n";
+    out << '\t' << message.name << "(const " << message.name << " &obj) {}\n";
     // move constructor
     out << "#if __cplusplus >= 201103L\n";
-    out << '\t' << className << "(const " << className << " &&obj) {}\n";
+    out << '\t' << message.name << "(const " << message.name << " &&obj) {}\n";
     out << "#endif\n";
     // destructor
     generateDtor(out, message);
     // assign operator
-    out << '\t' << className << " &operator=(const " << className << " &obj) { return *this; }\n";
+    out << '\t' << message.name << " &operator=(const " << message.name << " &obj) { return *this; }\n";
     // equality operator
-    out << "\tbool operator==(const " << className << " &obj) { return false; }\n\n";
+    out << "\tbool operator==(const " << message.name << " &obj) { return false; }\n\n";
 
     for (auto it = message.fields.begin(); it != message.fields.end(); ++it)
     {
@@ -232,6 +265,10 @@ static void generateMessage( std::ostream &out, const Message &message )
         out << "\t#define " << toUpper(it->name) << "_ID" << "  " << it->index << "\n";
         out << "\t#define " << toUpper(it->name) << "_NAME  \"" << it->name << "\"\n";
         out << "#endif\n";
+        // 'has' function
+        generateHasField(out, *it);
+        // 'clear' function
+        generateClearField(out, *it);
         // accessor functions
         generateFunctions(out, *it);
         out << '\n';
@@ -256,9 +293,10 @@ static void generateModel( std::ostream &out, const Proto3 &proto )
     out << "#include <stdint.h>\n\n";
 
     // forward declarations
+    out << "// forward declarations\n";
     for (auto it = proto.messages.begin(); it != proto.messages.end(); ++it)
     {
-        out << "// forward declarations\ntemplate<int> class " << it->name << "__type;\n";
+        out << "\nclass " << it->name << ";\n";
     }
 
     // message declarations
@@ -266,8 +304,8 @@ static void generateModel( std::ostream &out, const Proto3 &proto )
         generateMessage(out, *it);
 
     // public types
-    for (auto it = proto.messages.begin(); it != proto.messages.end(); ++it)
-        out << "class " << it->name << " : public " << it->name << "__type<0> { };\n";
+    //for (auto it = proto.messages.begin(); it != proto.messages.end(); ++it)
+    //    out << "class " << it->name << " : public " << it->name << "__type<0> { };\n";
 
     out << "#endif\n";
 }
