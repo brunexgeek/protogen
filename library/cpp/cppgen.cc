00fa52874012dc7805eec986f58a74f6356dfcd5
Bruno Ribeiro <brunexgeek@users.noreply.github.com>
2019-01-18 13:30:12 -0200

    Add license for generated code

--- library/cpp/cppgen.cc
+++ library/cpp/cppgen.cc
@@ -576,11 +576,44 @@ static std::string makeGuard( const std::string &fileName )
 }
 
 
+static void generateLicense( Printer &printer )
+{
+    printer(
+        "/*\n"
+        " * This is free and unencumbered software released into the public domain.\n"
+        " * \n"
+        " * Anyone is free to copy, modify, publish, use, compile, sell, or\n"
+        " * distribute this software, either in source code form or as a compiled\n"
+        " * binary, for any purpose, commercial or non-commercial, and by any\n"
+        " * means.\n"
+        " * \n"
+        " * In jurisdictions that recognize copyright laws, the author or authors\n"
+        " * of this software dedicate any and all copyright interest in the\n"
+        " * software to the public domain. We make this dedication for the benefit\n"
+        " * of the public at large and to the detriment of our heirs and\n"
+        " * successors. We intend this dedication to be an overt act of\n"
+        " * relinquishment in perpetuity of all present and future rights to this\n"
+        " * software under copyright law.\n"
+        " * \n"
+        " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,\n"
+        " * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n"
+        " * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.\n"
+        " * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR\n"
+        " * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\n"
+        " * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR\n"
+        " * OTHER DEALINGS IN THE SOFTWARE.\n"
+        " * \n"
+        " * For more information, please refer to <http://unlicense.org>\n */\n\n");
+}
+
+
 static void generateModel( Printer &printer, const Proto3 &proto )
 {
     char version[8] = { 0 };
     snprintf(version, sizeof(version) - 1, "%02X%02X%02X",
         (int) PROTOGEN_MAJOR, (int) PROTOGEN_MINOR, (int) PROTOGEN_PATCH);
+
+    generateLicense(printer);
 
     std::string guard = makeGuard(proto.fileName);
     printer(
