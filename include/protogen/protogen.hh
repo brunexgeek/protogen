49216a4a3256d1d7d44e4322fac359e370d9734a
Bruno Ribeiro <brunexgeek@users.noreply.github.com>
2021-11-01 11:58:06 -0300

    Fix Visual Studio compilation

--- include/protogen/protogen.hh
+++ include/protogen/protogen.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Bruno Ribeiro <https://github.com/brunexgeek>
+ * Copyright 2021 Bruno Ribeiro <https://github.com/brunexgeek>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
