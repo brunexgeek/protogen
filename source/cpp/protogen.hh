5742218bc6c4ea959e09eb60286c7986182eee17
Bruno Ribeiro <brunexgeek@users.noreply.github.com>
2024-12-14 15:24:01 -0300

    Update year in the license information

--- source/cpp/protogen.hh
+++ source/cpp/protogen.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Bruno Costa <https://github.com/brunexgeek>
+ * Copyright 2023-2024 Bruno Costa <https://github.com/brunexgeek>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
